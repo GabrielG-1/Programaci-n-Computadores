ee6c1b685252f439ba557fea1d33ba5bd6227b37
GabrielG-1 <83616104+GabrielG-1@users.noreply.github.com>
2021-05-17 17:32:08 -0400

    Add files via upload

---
+++ Código1.cpp
@@ -0,0 +1,12 @@
+int main() {
+  int num;
+  printf("Ingrese un número:");
+  scanf("%d", &num);
+  if (num %2 == 0){
+    printf("El número es par");
+  }
+  else {
+    printf("El número es impar");
+  }
+    return 0;
+}
