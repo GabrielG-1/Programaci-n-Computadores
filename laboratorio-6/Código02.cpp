2771706185840772f67b6144e11a5709dcd35be3
Gabriel Garzon <g.garzon01@ufromail.cl>
2021-05-17 20:54:38 -0500

    Código02.cpp

---
+++ laboratorio-6/Código02.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int n1, n2, n3;
+
+int main() {
+ printf("Ingrese un número", n1);
+ scanf(%d, &n1);
+ printf("Ingrese otro segundo número", n2);
+ scanf(%d, &n2);
+ printf("Ingrese un tercer número", n3);
+ scanf(%d, &n3);
+ if (n1>=n2 && n1>n3){
+     printf("EL número mayor es:" "%d", n1);
+ } else if (n2>n1 && n2>3)
+ {
+     printf("el número mayor es: %d ", n2);
+ } else if (n3>n2 && n3>n2);
+ printf("el número mayor es: %d", n3);
+ 
+}
